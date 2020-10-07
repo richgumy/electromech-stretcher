e7d7cc2f5a36df703db63b30711bb435e8ffd650
Richie Ellingham <rje69@uclive.ac.nz>
2020-10-07 18:46:56 +1300

    reads to resistance and time data to data.csv and plots relationship

--- readOhmmeter.py
+++ readOhmmeter.py
@@ -1,34 +1,35 @@
+import csv
 import pyvisa
 import time
-start_time = time.time()
+import matplotlib.pyplot as plt
 
 rm = pyvisa.ResourceManager()
 available_devs = rm.list_resources()
 inst = rm.open_resource(available_devs[0])
 # print(inst.query("*IDN?"))
-buf_size = 5
+buf_size = 10
 res_vals = []
 sample_times = []
+time_difs = []
 i = 0
+
+start_time = time.time()
 
 while(i < buf_size):
     t_s = time.time()
-    res_vals.append(inst.query(":MEASure:RESistance? (@101)"))
+    res_vals.append(float((inst.query(":MEASure:RESistance? (@101)")).strip()[1:-4]))
     t_f = time.time()
-    t_dif = t_f - t_s
+    t_dif = (t_f + t_s)/2-start_time
     sample_times.append(t_dif)
     i = i + 1
-
-# sum_res = 0
-# sum_time = 0
-# for i in range(len(sample_times)):
-#     sum_res = sum_res + int(res_vals[i])
-#     sum_time = sum_time + int(sample_times[i])
-#
-# average_res = sum_res/len(res_vals)
-# average_time = sum_time/len(sample_times)
+with open('data.csv', 'w', newline='') as csvfile:
+    data = csv.writer(csvfile, delimiter=',')
+    for i in range(len(sample_times)):
+        data.writerow([sample_times[i],res_vals[i]])
 
 print(res_vals)
 print(sample_times)
-# print(average_res)
-# print(average_time)
+plt.plot(sample_times, res_vals,'ro')
+plt.xlabel('Time[s]')
+plt.ylabel('Resistance[M Ohms]')
+plt.show()
