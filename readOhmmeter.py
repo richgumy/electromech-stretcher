2e3094811b9d82eb1708100a98a3ede6ddc22d55
Richie Ellingham <rje69@uclive.ac.nz>
2020-10-19 18:13:34 +1300

    serial comms with 34970a is now up to 2-5ms per sample with periodic errors. Errors potentially from error buffer overflow, yet to investigate.

--- readOhmmeter.py
+++ readOhmmeter.py
@@ -4,6 +4,28 @@ import pyvisa
 import serial
 import time
 
+# Ideas/issues for speeding up serial comms:
+# 1 - change trigger count (TRIG COUNT 1) to get rid of timeout error
+# 2 - FORUM POST[Is there any way, so that I can set instrument in continuous
+# scan mode and I read data periodically (say at interval of 100ms).
+# > I tried same with IOMonitor, if I send INIT+FETCH with Command Expert, it
+# takes 1ms + 1.3ms, but my code is taking ~180ms, why there is such difference?]
+# 3 - Change param using SCPI cmd :
+# :ROUT:SCAN (@101)
+# :ROUT:CHAN:DEL 0,(@101) -> delay between samples min =0 max = 60s
+# :SENSE:VOLT:DC:RANG 10000000,(@101) -> Set ranging (Auto is slower?)
+# :SENS:VOLT:DC:APER .0004,(@101) -> Set aperture time to min? = 400us
+# :SENSE:VOLT:DC:APER 0.00001,(@101) -> Set sample time 100ms
+# :TRIG:COUNT 1
+# :INIT
+# FETCH?
+# Additional read information:
+# :FORM:READ:TIME ON
+# :FORM:READ:TIME:TYPE REL or ABS
+# Go through "Agilent 34970a Quick Reference Guide" document
+#
+
+
 # Setup Serial arduino connection
 ardSerial = serial.Serial()
 
@@ -11,53 +33,82 @@ ardSerial = serial.Serial()
 rm = pyvisa.ResourceManager()
 available_devs = rm.list_resources()
 print(available_devs)
-device_index = input("Which device from the list (eg. 0 or 1 or 2 or...):")
-inst = rm.open_resource(available_devs[int(device_index)])
+# device_index = input("Which device from the list (eg. index 0 or 1 or 2 or ...):")
+# inst = rm.open_resource(available_devs[int(device_index)])
+inst = rm.open_resource(available_devs[2])
+inst.baud_rate = 115200
 inst.timeout = 5000
-inst.read_termination = '\r'
+# inst.read_termination = '\r'
+inst.write(":CONF:RES 10000000,1000,(@101)") #set PLC to 0.02 for 400us integration time... or higher for better noise suppression
+print("Resistance range set to 10MOhm, 1kOhm resolution")
+print(inst.query(":CONF? (@101)"))
 
-buf_size = 20
+inst.write(":ROUT:CHAN:DEL 0,(@101)")
+# print("delay set 0")
+# print(inst.query(":ROUT:CHAN:DEL? (@101)"))
+# print(inst.query(":CONF? (@101)"))
+# inst.read_termination = '\r'
+inst.write(":SENS:RES:NPLC 1,(@101)") #set PLC(i.e. 50Hz power line cycle for NZ) to 0.02 for 400us integration time... or higher for better noise suppression
+print("PLC set to %.2f" % float(inst.query(":SENS:RES:NPLC? (@101)")))
+
+
+inst.write("ROUT:SCAN (@101)") # Set scan channel
+inst.write(":TRIG:COUNT 1") # Set number of measurements taken each scan
+
+buf_size = 100
 res_vals = []
-sample_times = []
+avg_sample_times = [] #
+sample_times = [] # time taken for sample
 time_difs = []
+current_res = '0' # init start resistance to zero string
 i = 0
-
 
 mode = input("Press 'm' for manual mode any key for auto mode: ")
 if mode == 'm':
     while(i < buf_size):
-        ready = input("Press 'y' to continue take next sample: ")
+        ready = input("Press 'y' to continue to take next sample: ")
         start_time = time.time()
         if ready == 'y':
             t_s = time.time()
-            res_vals.append(float((inst.query(":MEASure:RESistance? (@101)")).strip()[1:-4]))
+            res_vals.append(float((inst.query(":MEAS:RES? (@101)")).strip()[1:-4]))
             t_f = time.time()
-            t_dif = (t_f + t_s)/2-start_time
-            sample_times.append(t_dif)
+            # t_dif = (t_f + t_s)/2-start_time
+            sample_times.append(t_s)
             i = i + 1
         else:
             break
         ready = ''
 else:
-        start_time = time.time()
+        start_time = time.time() # Record start time to make measurements relative not absolute
         while(i < buf_size):
             t_s = time.time()
-            current_res = inst.query(":MEASure:RESistance? (@101)")
-            # print(current_res[-1:])
-            res_vals.append(float(current_res.strip()[1:-4])*10**float(current_res[-1:]))
+            inst.write(":INIT") # Begin scan
+            time.sleep(0.002) # Wait for scan to finish
+            while len(current_res) <= 4: # If scan not finished keep questioning until result appears
+                current_res = inst.query("R?")
+            # print(current_res)
+            res_vals.append(current_res)
             t_f = time.time()
-            t_dif = (t_f + t_s)/2-start_time
-            sample_times.append(t_dif)
+            t_avg = (t_f + t_s)/2-start_time # Record time of measurement halfway between when measurement was requested and when it was received
+            avg_sample_times.append(t_avg)
+            t_d = t_f - t_s # How long did it take to receive the message from time of request
+            sample_times.append(t_d)
             i = i + 1
+print("Delay\nRange and res\nPLC..")
+print(inst.query(":ROUT:CHAN:DEL? (@101)"))
+print(inst.query(":CONF? (@101)"))
+print(inst.query(":SENS:RES:NPLC? (@101)"))
 
 with open('data.csv', 'w', newline='') as csvfile:
     data = csv.writer(csvfile, delimiter=',')
     for i in range(len(sample_times)):
+        res_vals[i] = float(res_vals[i].strip()[5:-4])*10**float(res_vals[i].strip()[-1:])
         data.writerow([sample_times[i],res_vals[i]])
 
 print(res_vals)
 print(sample_times)
-plt.plot(sample_times, res_vals,'ro')
+print(avg_sample_times)
+plt.plot(avg_sample_times, res_vals,'ro')
 plt.xlabel('Time[s]')
 plt.ylabel('Resistance[Ohms]')
 plt.show()
--- read_loadcell_NI_DAQ.py
+++ read_loadcell_NI_DAQ.py
@@ -4,13 +4,20 @@ from nidaqmx.constants import LineGrouping
 from nidaqmx.constants import Edge
 from nidaqmx.constants import AcquisitionType
 import matplotlib.pyplot as plt
+import time
 
 #keep the plat from closing between data sets
-plt.ion()
+# plt.ion()
 
 i = 0
+force_data = []
+time_data = []
 
 #calibration factors found through NI MAX: y = 0.456x - 0.364
+# electrical_vals= [-2.99, 7.7, 17.9, 23.7, 28]
+# physical_vals=[-0.5 ,0, 0.476, 0.75, 0.949]
+# Rated output = 1 +/- 0.15 mV/V
+# (1mV/V) / (10kg)??
 
 #code portion to read from the NI instrument
 with nidaqmx.Task() as task:
@@ -21,21 +28,25 @@ with nidaqmx.Task() as task:
         bridge_config=nidaqmx.constants.BridgeConfiguration.FULL_BRIDGE,
         voltage_excit_source= nidaqmx.constants.ExcitationSource.INTERNAL,
         voltage_excit_val=2.5, nominal_bridge_resistance=1000.0,
-        electrical_vals= [-2.99, 7.7, 17.9, 23.7, 28],
+        electrical_vals= [0, 1, 2, 3, 4],
         electrical_units=nidaqmx.constants.BridgeElectricalUnits.M_VOLTS_PER_VOLT,
-        physical_vals=[-0.5 ,0, 0.476, 0.75, 0.949],
-physical_units=nidaqmx.constants.BridgePhysicalUnits.KILOGRAM_FORCE,
+        physical_vals=[-1, 0, 1, 2, 3],
+physical_units=nidaqmx.constants.BridgePhysicalUnits.NEWTONS,
         custom_scale_name=None)
     task.timing.cfg_samp_clk_timing(100, source="",
     active_edge=Edge.RISING, sample_mode=AcquisitionType.FINITE, samps_per_chan=30)
     print("Loadcell configuration = successful!\n")
-    while i<50:
-        data = task.read(30)
-        print("Data received!")
-        plt.scatter(i,data[0],c='r')
+    start_time = time.time()
+    while i<100:
+        raw_data = task.read(1)
+        force = 452.29*float(raw_data[0]) + 98.155
+        force_data.append(force)
+        time_i = time.time() - start_time
+        time_data.append(time_i)
         #plt.scatter(i,data[1],c='b')
-        plt.pause(0.01)
-        plt.show()
+        # plt.pause(0.01)
         i=i+1
-        print(data)
+        print(force)
+
+plt.scatter(time_data,force_data,c='r')
 plt.show()
